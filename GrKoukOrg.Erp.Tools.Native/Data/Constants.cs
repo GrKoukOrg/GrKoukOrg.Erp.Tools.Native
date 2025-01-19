a80e217641ab94b3555bb81b11793e30a6ad202b
George Koukoudis <grkouk@hotmail.com>
2025-01-19 16:22:27 +0200

    Changed SqLite Db Name

--- GrKoukOrg.Erp.Tools.Native/Data/Constants.cs
+++ GrKoukOrg.Erp.Tools.Native/Data/Constants.cs
@@ -2,7 +2,7 @@ namespace GrKoukOrg.Erp.Tools.Native.Data
 {
     public static class Constants
     {
-        public const string DatabaseFilename = "AppSQLite.db3";
+        public const string DatabaseFilename = "ErpToolsNativeSQLite.db3";
 
         public static string DatabasePath =>
             $"Data Source={Path.Combine(FileSystem.AppDataDirectory, DatabaseFilename)}";
