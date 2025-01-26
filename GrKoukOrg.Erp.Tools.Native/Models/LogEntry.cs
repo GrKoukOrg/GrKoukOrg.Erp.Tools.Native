8378ae41d4af0cf5f96c576586a47ecd8a2e13a7
George Koukoudis <grkouk@hotmail.com>
2025-01-27 01:08:42 +0200

    Step

---
+++ GrKoukOrg.Erp.Tools.Native/Models/LogEntry.cs
@@ -0,0 +1,7 @@
+namespace GrKoukOrg.Erp.Tools.Native.Models;
+
+public class LogEntry
+{
+    public string Timestamp { get; set; }
+    public string Message { get; set; } 
+}
--- GrKoukOrg.Erp.Tools.Native/PageModels/SyncItemsPageModel.cs
+++ GrKoukOrg.Erp.Tools.Native/PageModels/SyncItemsPageModel.cs
@@ -1,54 +1,52 @@
+using System.Collections.ObjectModel;
 using System.Text;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using GrKoukOrg.Erp.Tools.Native.Models;
-
 using Microsoft.Extensions.Logging;
 
 namespace GrKoukOrg.Erp.Tools.Native.PageModels;
 
-public partial class SyncItemsPageModel: ObservableObject
+public partial class SyncItemsPageModel : ObservableObject
 {
     private readonly LocalItemsRepo _localItemsRepo;
     private readonly IServerDataAccess _serverDataAccess;
     private readonly ILogger<SyncItemsPageModel> _logger;
     private readonly IHttpClientFactory _httpClientFactory;
 
-    private readonly StringBuilder _logMessages = new();
-    [ObservableProperty]
-    private string logMessages = string.Empty;
+
+    [ObservableProperty] private int _operationProgress = 0;
+
+    [ObservableProperty] [NotifyCanExecuteChangedFor(nameof(AddItemsToLocalDatabaseCommand))]
+    private bool _canSyncItems = false;
+
+    [ObservableProperty] private ICollection<ItemListDto> _items = new List<ItemListDto>();
+
+    [ObservableProperty] private int _itemCount = 0;
+    public ObservableCollection<LogEntry> LogEntries { get; } = new();
 
     private void AddLog(string message)
     {
-        _logMessages.AppendLine($"{DateTime.Now}: {message}");
+        LogEntries.Add(new LogEntry { Timestamp = DateTime.Now.ToString("HH:mm:ss"), Message = message });
     }
 
-    private void UpdateLog()
-    {
-         LogMessages = _logMessages.ToString();
-    }
-
-    [ObservableProperty]
-    private bool _isConnected = false;
-    
-    [ObservableProperty]
-    private ICollection<ItemListDto> _items = new List<ItemListDto>();
-
-    [ObservableProperty]
-    private int _itemCount = 0;
-
-    public SyncItemsPageModel(LocalItemsRepo localItemsRepo,IServerDataAccess serverDataAccess, ILogger<SyncItemsPageModel> logger)
+    public SyncItemsPageModel(LocalItemsRepo localItemsRepo, IServerDataAccess serverDataAccess,
+        ILogger<SyncItemsPageModel> logger)
     {
         _localItemsRepo = localItemsRepo;
         _serverDataAccess = serverDataAccess;
         _logger = logger;
     }
-    
+
+    partial void OnItemCountChanged(int value)
+    {
+        CanSyncItems = value > 0;
+    }
+
     [RelayCommand]
     private async Task Appearing()
     {
         //Projects = await _projectRepository.ListAsync();
-        
     }
 
     [RelayCommand]
@@ -60,24 +58,20 @@ public partial class SyncItemsPageModel: ObservableObject
             Items = await _serverDataAccess.GetServerItemsListAsync();
             ItemCount = Items.Count;
             AddLog($"Connected and retrieved {ItemCount} items");
-            UpdateLog();
-           // await AppShell.DisplayToastAsync("Connected and retrieved items");
+            // await AppShell.DisplayToastAsync("Connected and retrieved items");
         }
         catch (Exception e)
         {
             _logger.LogError(e, "Error in GetItems");
             AddLog($"Error in GetItems: {e.Message}");
-            UpdateLog();
         }
-       
-       
     }
 
-    [RelayCommand]
+    [RelayCommand(CanExecute = nameof(CanSyncItems))]
     private async Task AddItemsToLocalDatabase()
     {
         AddLog("Adding items to local database");
-        UpdateLog();
+
         foreach (var (item, index) in Items.Select((item, index) => (item, index)))
         {
             //Check if item with Id already exists in the database
@@ -85,20 +79,16 @@ public partial class SyncItemsPageModel: ObservableObject
             {
                 var result = await _localItemsRepo.UpdateItemAsync(item);
                 AddLog($"Index: {index}, Id: {item.Id}, Name: {item.Name} Update item result returned {result}");
+                
             }
             else
             {
                 var result = await _localItemsRepo.AddItemAsync(item);
                 AddLog($"Index: {index}, Id: {item.Id}, Name: {item.Name} Add item result returned {result}");
             }
-
-            if (index % 10 == 0)
-            {
-                UpdateLog();
-            }
-            
+            OperationProgress=index;
         }
-        UpdateLog();
+
         await AppShell.DisplayToastAsync("Items added and updated to local database");
     }
 }
