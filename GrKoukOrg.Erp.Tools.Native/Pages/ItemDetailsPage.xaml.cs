ae78a7609c2e46b00abaa4a99936d4ac02ff6fae
George Koukoudis <grkouk@hotmail.com>
2025-02-03 12:16:36 +0200

    Start testing barcode scanner

--- GrKoukOrg.Erp.Tools.Native/Pages/ItemDetailsPage.xaml.cs
+++ GrKoukOrg.Erp.Tools.Native/Pages/ItemDetailsPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommunityToolkit.Maui.Views;
 using SelectionChangedEventArgs = Syncfusion.Maui.Inputs.SelectionChangedEventArgs;
 
 namespace GrKoukOrg.Erp.Tools.Native.Pages;
@@ -15,5 +16,10 @@ public partial class ItemDetailsPage : ContentPage
         InitializeComponent();
     }
 
-   
+
+    private async void Button_OnClicked(object? sender, EventArgs e)
+    {
+        var barcodeScannerPopupPage = new BarCodeScannerPopupPage();
+        var result = await this.ShowPopupAsync(barcodeScannerPopupPage, CancellationToken.None);
+    }
 }
