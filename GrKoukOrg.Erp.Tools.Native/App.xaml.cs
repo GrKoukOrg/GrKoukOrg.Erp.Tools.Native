8a25f59c465b18ddea162046dc43bcd3759f8a65
George Koukoudis <grkouk@hotmail.com>
2025-02-27 01:08:19 +0200

    Set application culture to "el-GR" and update data grid columns.
    Configured the application to use Greek culture for date, number, and UI formatting. Added new columns to the data grid, including hidden Id, date, and formatted amount fields, while refining existing column definitions.

--- GrKoukOrg.Erp.Tools.Native/App.xaml.cs
+++ GrKoukOrg.Erp.Tools.Native/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace GrKoukOrg.Erp.Tools.Native
+﻿using System.Globalization;
+
+namespace GrKoukOrg.Erp.Tools.Native
 {
     public partial class App : Application
     {
@@ -6,6 +8,10 @@
         {
             Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MzY2OTAwM0AzMjM4MmUzMDJlMzBpV0RjOFR3VzhPL0RFSU8xT1lIVjJFVktHR0k1Y3M1ZUYxOVNyNTZMTldFPQ==");
             InitializeComponent();
+            var culture = new CultureInfo("el-GR");
+            Thread.CurrentThread.CurrentCulture = culture;
+            Thread.CurrentThread.CurrentUICulture = culture;
+
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
